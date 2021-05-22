1f6f71551f030263eb126783b556004628491bcc
ybotwe <ybotwe@gmail.com>
2021-05-22 17:20:14 +0000

    Initial commit
    This is the initial code template for the udacity project FlightSurety.

--- truffle-config.js
+++ truffle-config.js
@@ -1,11 +1,11 @@
 var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+var mnemonic = "tissue buyer coffee clinic traffic grain sorry grain match project secret ten";
 
 module.exports = {
   networks: {
     development: {
       provider: function() {
-        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
+        return new HDWalletProvider(mnemonic, "http://127.0.0.1:7545/", 0, 100);
       },
       network_id: '*',
       gas: 9999999
